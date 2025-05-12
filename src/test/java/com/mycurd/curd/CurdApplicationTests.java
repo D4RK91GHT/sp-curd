c423323dd1a0f30f717e128641f0312b55dc6e74
D4RK91GHT <rahulmajumdar400@gmail.com>
2025-05-13 00:03:32 +0530

    first commit -m

---
+++ src/test/java/com/mycurd/curd/CurdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycurd.curd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
